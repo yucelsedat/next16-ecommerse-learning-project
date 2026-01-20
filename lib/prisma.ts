b4d7330d2908030f360cb49e154f194e39f670e8
yucelsedat <yucelsedatt@gmail.com>
2026-01-20 21:22:36 +0300

    fix(prisma): Prisma Client Singleton for Next16

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,15 @@
-import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-export { prisma };
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
