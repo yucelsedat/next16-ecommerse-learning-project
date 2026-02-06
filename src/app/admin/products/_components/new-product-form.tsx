e0c7829cb20f331b83f5a5b2d137fa7dbe6a38a4
yucelsedat <yucelsedatt@gmail.com>
2026-02-06 17:51:38 +0300

    feat(admin-product): input file uploading progress animation & img preview

--- src/app/admin/products/_components/new-product-form.tsx
+++ src/app/admin/products/_components/new-product-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { cn } from "@/lib/utils"
 import { useActionState, useEffect, useRef, useState, useTransition } from "react"
 import { ActionState, addProduct } from '../../_actions/products'
 import { Label } from "@/components/ui/label"
@@ -11,7 +10,6 @@ import { Button } from "@/components/ui/button"
 import { Image, Loader2 } from "lucide-react"
 import { Progress } from "@/components/ui/progress"
 import { useRouter } from "next/navigation"
-import { error } from "console"
 
 const initialState:ActionState = {}
 
@@ -28,22 +26,59 @@ export default function NewProductForm() {
   }, [state.success, router])  
 
   const [priceInCents, setPriceInCents] = useState<number>()
-  const [uploadProgress, setUploadProgress] = useState<number>(15)
+
   const [fileName, setFileName] = useState<string>('Click to upload')
+  const [uploadProgress, setUploadProgress] = useState<number>(15)
+  const [preview, setPreview] = useState<string | null>(null)
+  const [uploading, setUploading] = useState(false)
+  // daha sonra real img update işlemlerinde kullanılacaktır 
+  const [isPending, startTransition] = useTransition()
+
+  
 
   const fileRef = useRef<HTMLInputElement | null>(null)
-
-  const isUploading = false
-  const [isPending, startTransition] = useTransition()
 
   const handleClick = () => {
     fileRef.current?.click()
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
     if (e.target.files?.[0]) {
       setFileName(e.target.files[0].name)
+      handleFileChange(e.target.files[0])
     }
+  }
+
+  const handleFileChange = (file: File) => {
+
+    const reader = new FileReader()
+
+    setUploading(true)
+    setUploadProgress(0)
+
+    reader.onprogress = (event) => {
+      if (!event.lengthComputable) return
+      const percent = Math.round((event.loaded * 100) / event.total)
+      setUploadProgress(percent)
+    }
+
+    const MIN_DURATION = 400 // ms
+    const start = performance.now()
+
+    reader.onloadend = () => {
+
+      const elapsed = performance.now() - start
+      const remaining = Math.max(0, MIN_DURATION - elapsed)
+
+      setTimeout(() => {
+        setPreview(reader.result as string)
+        setUploading(false)
+      }, remaining)
+
+    }
+
+    reader.readAsDataURL(file)
   }
 
   return (
@@ -52,45 +87,67 @@ export default function NewProductForm() {
       <div className="grid grid-cols-3">
         {/* flex-1 */}
         <div
-          className='relative h-full flex-1 mx-auto w-2/3 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center hover:bg-gray-900/8' 
+          className='relative h-80 flex-1 mx-auto w-2/3 rounded-xl bg-gray-900/5 px-2 py-auto ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center hover:bg-gray-900/8 hover:cursor-pointer' 
           onClick={handleClick}
         >
+        {preview
+          ? 
+            <div>
+              <img 
+              src={preview}
+              alt="Preview"
+              className="w-full rounded-md border"
+              /> 
+              <input 
+                ref={fileRef} 
+                className='hidden' 
+                type="file" 
+                name="image" 
+                onChange={handleChange}
+                // required
+              />
+            </div>
+          : (
+            
           <div className="relative flex flex-1 my-32 flex-col items-center justify-center w-full">
-            {isUploading || isPending ? <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
-            : <Image className="h-6 w-6 text-zinc-500 mb-2"/>}
-            <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
-                {
-                  isUploading 
-                    ? <div className="flex flex-col items-center">
-                        <p>Uploading...</p>
-                        {/* buraya progress bar animasyonu koyalım */}
-                        <Progress value={uploadProgress} className="mt-2 w-40 h-2 bg-gray-300"/>
-                      </div> 
-                    : isPending 
-                      ? <div>
-                          <p>Redirecting, please wait...</p>
-                      </div> 
-                      : <div >
-                        {fileName}
-                        <input 
-                          ref={fileRef} 
-                          className='hidden' 
-                          type="file" 
-                          name="image" 
-                          onChange={handleChange}
-                          // required
-                        />
-                      </div>
-                }
-
-              </div>
-              {isPending 
-                ? null 
-                : <p className="text-xs text-zinc-500 ">
-                  PNG, JPG, JPEG
-                </p>
+          {uploading || isPending ? <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+          : <Image className="h-6 w-6 text-zinc-500 mb-2"/>}
+          <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+              {
+                uploading 
+                  ? <div className="flex flex-col items-center">
+                      <p>Uploading...</p>
+                      {/* buraya progress bar animasyonu koyalım */}
+                      <Progress value={uploadProgress} className="mt-2 w-40 h-2 bg-gray-300"/>
+                    </div> 
+                  : isPending 
+                    ? <div>
+                        <p>Redirecting, please wait...</p>
+                    </div> 
+                    : <div >
+                      {fileName}
+                      <input 
+                        ref={fileRef} 
+                        className='hidden' 
+                        type="file" 
+                        name="image" 
+                        onChange={handleChange}
+                        // required
+                      />
+                    </div>
               }
-          </div>
+
+            </div>
+            {isPending 
+              ? null 
+              : <p className="text-xs text-zinc-500 ">
+                PNG, JPG, JPEG
+              </p>
+            }
+        </div>
+      
+          )
+        }
         </div>
         {/* col2 + col3 */}
         <div className="col-span-2 flex flex-col justify-between">
@@ -116,7 +173,7 @@ export default function NewProductForm() {
         <Button className="place-self-center" disabled={isActionPending}>
         {isActionPending ? 'Kaydediliyor…' : 'Ürün Ekle'}
         </Button>
-        <span className="place-self-center">
+        <span className="place-self-center text-red-500">
           {/* {JSON.stringify(state)} */}
           {JSON.stringify(state?.message)??JSON.stringify(state?.errors?.image[0])}
         </span>
